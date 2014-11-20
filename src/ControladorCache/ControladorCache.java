fbd1f4943a4508a593c3e990ffb589879d18a412
Eduardo Benjamín <eduardo_benji@hotmail.com>
2014-11-20 16:10:24 -0600

    Tiene los cambios que hicimos en la biblio.

---
+++ src/ControladorCache/ControladorCache.java
@@ -0,0 +1,61 @@
+package ControladorCache;
+
+import org.apache.jcs.JCS;
+import org.apache.jcs.access.exception.CacheException;
+
+/**
+ *
+ * @author Lalo
+ */
+public class ControladorCache {
+
+    private JCS jcsCache;
+
+    public ControladorCache() {
+    }
+
+    /**
+     * Este método es el encargado de cargar el archivo de configuración de la
+     * caché. Si se desea manipular la caché, será este método que se deba
+     * llamar en primer lugar.
+     */
+    public void configLoad() {
+        try {
+            // Se carga el cache usando el archivo de configuracion
+            jcsCache = JCS.getInstance("mvcDetailsCache");
+        } catch (CacheException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+
+    /**
+     *Método encargado de agregar la información del candidato a la caché.
+     * @param id es el identificador del objeto que se introducirá a la caché.
+     * @param candidato es el objeto que se introducirá a la caché.
+     */
+    public void put(String id, Object candidato) {
+
+        try {
+            jcsCache.put(id, candidato);
+            
+        } catch (CacheException ex) {
+            System.out.println("**ERROR!**");
+            ex.printStackTrace();
+        }
+
+    }
+
+    /**
+     * Método encargado de devolver de la caché, el candidato que se desea, a
+     * partir de la clave que se le pase.
+     *
+     * @param claveCandidato, que es el identificador del candidato que se desea
+     * obtener de la caché.
+     * @return el objeto del candidato.
+     */
+    public Object get(String claveCandidato) {
+        
+        return  jcsCache.get(claveCandidato);
+    }
+}
