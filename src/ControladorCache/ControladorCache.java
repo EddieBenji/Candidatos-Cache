76c6fa70918452df95d0b3c32f05670d2048fc0f
Eduardo Benjamín <eduardo_benji@hotmail.com>
2014-11-24 23:09:27 -0600

    Se implementó para que generara un jar, que sería usado en la implementación del MVC.

--- src/ControladorCache/ControladorCache.java
+++ src/ControladorCache/ControladorCache.java
@@ -32,12 +32,12 @@ public class ControladorCache {
     /**
      *Método encargado de agregar la información del candidato a la caché.
      * @param id es el identificador del objeto que se introducirá a la caché.
-     * @param candidato es el objeto que se introducirá a la caché.
+     * @param objeto es el objeto que se introducirá a la caché.
      */
-    public void put(int id, Object candidato) {
+    public void put(int id, Cacheable objeto) {
         String ID = String.valueOf(id);
         try {
-            jcsCache.put(ID, candidato);
+            jcsCache.put(ID, objeto);
             
         } catch (CacheException ex) {
             System.out.println("**ERROR!**");
@@ -50,12 +50,27 @@ public class ControladorCache {
      * Método encargado de devolver de la caché, el candidato que se desea, a
      * partir de la clave que se le pase.
      *
-     * @param claveCandidato, que es el identificador del candidato que se desea
+     * @param claveObjeto, que es el identificador del candidato que se desea
      * obtener de la caché.
      * @return el objeto del candidato.
      */
-    public Object get(int claveCandidato) {
-        String id = String.valueOf(claveCandidato);
-        return  jcsCache.get(id);
+    public Cacheable get(int claveObjeto) {
+        String id = String.valueOf(claveObjeto);
+        return  (Cacheable) jcsCache.get(id);
     }
+    
+    /**
+     * Método encargado de limpiar la caché. (la deja sin datos completamente)
+     */
+    public void limpiarCache() {
+        try {
+            jcsCache.clear();
+        } catch (CacheException ex) {
+            System.out.println("**ERROR!**");
+            ex.printStackTrace();
+        }
+    }
+    
+    
+    
 }
