a8025a790c715922cd0398a06eac7635911d9aa0
Eduardo Benjamín <eduardo_benji@hotmail.com>
2014-11-28 11:53:35 -0600

    Se hizo singleton..!

--- src/Clases/ControladorCache.java
+++ src/Clases/ControladorCache.java
@@ -11,31 +11,43 @@ public class ControladorCache {
 
     private JCS jcsCache;
 
-    public ControladorCache() {
+    private static ControladorCache instanciaCache = new ControladorCache();
+
+    private ControladorCache() {}
+
+    /**
+     * Implementa el patrón de diseño singleton, devuelve una instancia única
+     * del controlador de la caché.
+     *
+     * @return instancia de la caché.
+     */
+    public static ControladorCache getInstanciaCache() {
+        return instanciaCache;
     }
 
     /**
      * Este método es el encargado de cargar el archivo de configuración de la
      * caché. Si se desea manipular la caché, será este método que se deba
      * llamar en primer lugar.
+     *
      * @throws org.apache.jcs.access.exception.CacheException
      */
     public void configLoad() throws CacheException {
 
-            // Se carga el cache usando el archivo de configuracion
-            jcsCache = JCS.getInstance("mvcDetailsCache");
+        // Se carga el cache usando el archivo de configuracion
+        jcsCache = JCS.getInstance("mvcDetailsCache");
     }
 
-
     /**
-     *Método encargado de agregar la información del candidato a la caché.
+     * Método encargado de agregar la información del candidato a la caché.
+     *
      * @param id es el identificador del objeto que se introducirá a la caché.
      * @param objeto es el objeto que se introducirá a la caché.
      * @throws org.apache.jcs.access.exception.CacheException
      */
     public void put(int id, Cacheable objeto) throws CacheException {
         String ID = String.valueOf(id);
-            jcsCache.put(ID, objeto);
+        jcsCache.put(ID, objeto);
     }
 
     /**
@@ -48,17 +60,16 @@ public class ControladorCache {
      */
     public Object get(int claveObjeto) throws CacheException {
         String id = String.valueOf(claveObjeto);
-        return  (Cacheable) jcsCache.get(id);
+        return (Cacheable) jcsCache.get(id);
     }
-    
+
     /**
      * Método encargado de limpiar la caché. (la deja sin datos completamente)
+     *
      * @throws org.apache.jcs.access.exception.CacheException
      */
     public void limpiarCache() throws CacheException {
-            jcsCache.clear();        
+        jcsCache.clear();
     }
-    
-    
-    
+
 }
